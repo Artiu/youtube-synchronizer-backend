fa83fcaeec1e89c16333a5e0643fc000ecc0afa2
Artiu <oborskiartur123@gmail.com>
2023-10-26 11:41:57 +0200

    Add example receiver test

--- receiver_test.go
+++ receiver_test.go
@@ -1,5 +1,17 @@
 package main
 
-func ReceiverTest() {
+import (
+	"net/http/httptest"
+	"testing"
+)
 
+func TestReceiver(t *testing.T) {
+	t.Run("test sse", func(t *testing.T) {
+		res := httptest.NewRecorder()
+
+		receiver := NewReceiverSSE(res, res)
+		receiver.SendHostDisconnectedMessage()
+
+		t.Log(res.Body.String())
+	})
 }
